f76e4b28c4cc43520134e52c7ff9ad59d266013f
Haozhe Li <hl83@illinois.edu>
2024-07-11 14:47:14 +0800

    feat: add function to update database from folder

---
+++ tests/update_picdb_from_folder.py
@@ -0,0 +1,8 @@
+import sys
+
+
+from core.update_db import update_db_from_folder
+from core.globalvaris import *
+
+folder = os.path.join(ROOT_DIR, "tests", "images_folder")# load all photos from this folder
+update_db_from_folder(folder)
